26b28af8903d4f289f5526a1b873480b133dee8b
AnaBratulescu <ana.bratulescu@tntcomputers.ro>
2020-10-22 11:47:23 +0300

    test

--- TNTManagerWebCronJobs/TNTManagerWebCronJobs/IHangfireEmailJob.cs
+++ TNTManagerWebCronJobs/TNTManagerWebCronJobs/IHangfireEmailJob.cs
@@ -10,13 +10,15 @@ namespace TNTManagerWebCronJobs
     public interface IHangfireEmailJob
     {
         [Queue("emails")] //Specifies that the job only runs on this server's queue. The queue attribute must be in the interface, not the implementation
-
         Task<string> SendDailyReport();
 
+        [Queue("emails")]
         Task<string> SendReminders();
 
+        [Queue("emails")]
         Task<string> SendWeeklyReport();
 
+        [Queue("emails")]
         Task<string> SendAlerts();
     }
 }
